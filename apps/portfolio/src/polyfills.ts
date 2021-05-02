d06728c20c21fcadda1f3f21818f0f66a1b4b200
AlexAegis <alexaegis@gmail.com>
2021-05-02 23:34:43 +0200

    feat: added scully

--- apps/portfolio/src/polyfills.ts
+++ apps/portfolio/src/polyfills.ts
@@ -55,8 +55,14 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone'; // Included with Angular CLI.
-
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+// tslint:disable-next-line: align
+import 'zone.js/dist/zone'; // Included with Angular CLI.
+/***************************************************************************************************
+ * SCULLY IMPORTS
+ * ! ORDER IS IMPORTANT
+ */
+import 'zone.js/dist/task-tracking';
